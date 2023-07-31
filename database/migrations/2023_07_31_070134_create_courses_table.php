c64879bde88fdbd7a8830c9bb00460f90d6556c2
rahatstaffasia <102013966+rahatstaffasia@users.noreply.github.com>
2023-07-31 16:33:33 +0600

    course resource

---
+++ database/migrations/2023_07_31_070134_create_courses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('description');
+            $table->string('image', 100)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
