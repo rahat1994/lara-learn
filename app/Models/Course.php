c8e5fcc3b338bf8b8c1b87fd6e38666d518f91a9
Rahat Baksh <rahat392@gmail.com>
2023-08-06 15:52:29 +0600

    course slugs

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
+    protected static ?string $recordTitleAttribute = 'title';
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+        'image',
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
