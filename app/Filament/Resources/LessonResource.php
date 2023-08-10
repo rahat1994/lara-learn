d2fa90d91482dd0b1186ad7fe84e9b2aa8d06252
Rahat Baksh <rahat392@gmail.com>
2023-08-10 23:09:44 +0600

    creating lesson resource and dropping course id

---
+++ app/Filament/Resources/LessonResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LessonResource\Pages;
+use App\Filament\Resources\LessonResource\RelationManagers;
+use App\Models\Lesson;
+use Filament\Forms;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LessonResource extends Resource
+{
+    protected static ?string $model = Lesson::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationGroup = 'Lara Learn';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')
+                    ->autofocus()
+                    ->required()
+                    ->maxLength(255)
+                    ->placeholder(__('forms.title')),
+                RichEditor::make('content')
+                    ->required()
+                    ->placeholder(__('forms.description')),
+                TextInput::make('video_url'),
+                Hidden::make('user_id')
+                    ->default(fn () => auth()->user()->id),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLesson::route('/create'),
+            'view' => Pages\ViewLesson::route('/{record}'),
+            'edit' => Pages\EditLesson::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Lesson.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'video_url',
+        'user_id',
+        'course_id',
+    ];
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
