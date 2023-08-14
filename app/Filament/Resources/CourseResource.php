204185736bafc0fb32fc823c08465e2f498f1e7a
Rahat Baksh <rahat392@gmail.com>
2023-08-14 23:04:01 +0600

    course resource reflect the updated database

--- app/Filament/Resources/CourseResource.php
+++ app/Filament/Resources/CourseResource.php
@@ -4,13 +4,22 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\CourseResource\Pages;
 use App\Filament\Resources\CourseResource\RelationManagers;
+use App\Forms\Components\LlDuration;
 use App\Models\Course;
 use AymanAlhattami\FilamentPageWithSidebar\FilamentPageSidebar;
 use AymanAlhattami\FilamentPageWithSidebar\PageNavigationItem;
 use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -50,6 +59,113 @@ class CourseResource extends Resource
                     ->directory('course_featured_images')
                     ->required()
                     ->placeholder(__('forms.featured_image')),
+                Section::make(__('course_admin.settings'))->schema([
+                    Tabs::make(__('course_admin.settings'))
+                        ->tabs([
+                            Tab::make(__('course_admin.general_settings'))->schema([
+                                LlDuration::make('duration')
+                                    ->datalist([
+                                        'Minutes',
+                                        'Hours',
+                                        'Days',
+                                        'Weeks',
+                                        'Months',
+                                        'Years',
+
+                                    ]),
+                                Checkbox::make('allow_repurchase')
+                                    ->default(false)
+                                    ->helperText(__('course_admin.allow_repurchase_helper_text')),
+                                Select::make('course_level')
+                                    ->helperText(__('course_admin.course_level_helper_text'))
+                                    ->options([
+                                        'All Levels' => 'All Levels',
+                                        'Beginner Level' => 'Beginner Level',
+                                        'Intermediate Level' => 'Intermediate Level',
+                                        'Expert Level' => 'Expert Level',
+                                    ]),
+                                Select::make('language')
+                                    ->helperText(__('course_admin.language_helper_text'))
+                                    ->options([
+                                        'English' => 'English',
+                                        'Arabic' => 'Arabic',
+                                        'French' => 'French',
+                                        'Spanish' => 'Spanish',
+                                    ]),
+                                TextInput::make('fake_students_enrolled')
+                                    ->helperText(__('course_admin.fake_students_enrolled_helper_text'))
+                                    ->numeric()->default(0),
+                                TextInput::make('max_students')
+                                    ->helperText(__('course_admin.max_students_helper_text'))
+                                    ->numeric(),
+                                TextInput::make('retake_course')
+                                    ->helperText(__('course_admin.retake_course_helper_text'))
+                                    ->numeric(),
+                                CheckBox::make('finish_button')
+                                    ->helperText(__('course_admin.finish_button_helper_text')),
+                                Checkbox::make('featured_list')
+                                    ->helperText(__('course_admin.featured_list_helper_text'))
+                                    ->default(false),
+                                Textarea::make('featured_review')
+                                    ->helperText(__('course_admin.featured_review_helper_text'))
+                                    ->rows(3)
+                                    ->placeholder(__('course_admin.featured_review_placeholder')),
+                                TextInput::make('external_link')->url()->helperText(__('course_admin.external_link_helper_text'))
+                            ]),
+                            Tab::make(__('course_admin.pricing'))->schema([
+                                TextInput::make('regular_price')
+                                    ->numeric()
+                                    ->default(0)
+                                    ->required(),
+                                TextInput::make('sale_price')
+                                    ->label(__('course_admin.sale_price'))
+                                    ->default(0)
+                                    ->numeric(),
+                                Checkbox::make('free_course')
+                                    ->default(false),
+                            ]),
+                            Tab::make(__('course_admin.extra_information'))->schema([
+                                Repeater::make(__('course_admin.requirements'))
+
+                                    ->schema([
+                                        TextInput::make('requirement')
+                                            ->placeholder(__('course_admin.requirement_placeholder'))
+                                            ->required(),
+                                    ]),
+                                Repeater::make(__('course_admin.target_audience'))
+
+                                    ->schema([
+                                        TextInput::make('target_audience')
+                                            ->placeholder(__('course_admin.requirement_placeholder'))
+                                            ->required(),
+                                    ]),
+                                Repeater::make(__('course_admin.key_features'))
+
+                                    ->schema([
+                                        TextInput::make('key_features')
+                                            ->placeholder(__('course_admin.key_feature_placeholder'))
+                                            ->required(),
+                                    ]),
+                            ]),
+                            Tab::make(__('course_admin.evaluation'))->schema([
+                                Radio::make('evaluation')
+                                    ->options([
+                                        'evaluate_via_lessons' => 'Evaluate via lessons',
+                                        'evaluate_via_results_of_the_final_quiz' => 'Evaluate via results of the final quiz',
+                                        'evaluate_via_passed_quizzes' => 'Evaluate via passed quizes',
+                                        'evaluate_via_questions' => 'Evaluate via questions',
+                                        'evaluate_via_mark' => 'Evaluate via mark',
+                                    ])
+                                    ->default('evaluate_via_lessons')
+                                    ->helperText(__('course_admin.evaluation_helper_text')),
+                                TextInput::make('pass_mark')
+                                    ->numeric()
+                                    ->default(80)
+                                    ->required(),
+                            ])
+                        ]),
+
+                ])->collapsible()->compact(),
                 Hidden::make('user_id')
                     ->default(fn () => auth()->user()->id),
             ])->columns(1);
---
+++ lang/en/course_admin.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Lesson Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'general_settings' => 'General Settings',
+    'settings' => 'Settings',
+    'allow_repurchase_helper_text' => 'Allow users to repurchase this course after it has been finished or blocked (Do not apply to free courses).',
+    'course_level_helper_text' => 'Choose a difficulty level.',
+    'language_helper_text' => 'Primary langage of this course.',
+    'fake_students_enrolled_helper_text' => 'This number will be added to the number of students enrolled in this course.',
+    'max_students_helper_text' => 'Maximum number of students allowed to enroll in this course.To disable, set to 0.',
+    'retake_course_helper_text' => 'The number of times a user can learn again from this course. To disable, set to 0.',
+    'finish_button_helper_text' => 'Allow showing the finish button when the student has completed all items but has not passed the course assessment yet.',
+    'featured_list_helper_text' => 'Add the course to the Featured List.',
+    'featured_review_helper_text' => 'A good review to promote the course.',
+    'external_link_helper_text' => 'Normally used for offline classes. Ex: link to a contact page. Format: https://google.com',
+    'preview_helper_text' => 'Check this box to make this lesson available for preview.',
+];
