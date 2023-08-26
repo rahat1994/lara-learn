cbd4f6c367084757a200a722609b44de8c698315
Rahat Baksh <rahat392@gmail.com>
2023-08-26 22:49:00 +0600

    work on radme.md and curriculum widget.

--- resources/views/filament/resources/course-resource/widgets/curriculums-widget.blade.php
+++ resources/views/filament/resources/course-resource/widgets/curriculums-widget.blade.php
@@ -1,15 +1,15 @@
 <x-filament::widget>
     <x-filament::card>
 
-        <div x-data="sort()" x-init="init()">
+        <div class="bg-gray-100" x-data="sort()" x-init="init()">
             <ul x-ref="items" id="items">
                 <template x-for="value in list" x-ref="list_template">
-                    <li class="px-2 py-1 flex flex-col" :data-id="value.id">
-                        <div class="basis-1" x-text="value.title"></div>
-                        <div class="basis-1" x-text="value.description"></div>
-                        <ul class="basis-1">
+                    <li class="px-3 py-2 mt-3 flex flex-col rounded border-2 border-inherit" :data-id="value.id">
+                        <div class="text-2xl subpixel-antialiased font-bold handle cursor-move" x-text="value.title"></div>
+                        <div class="text-sm  font-extralight" x-text="value.description"></div>
+                        <ul class="curriculum_modules p-4" x-bind:id="`curriculum_modules_no_`+value.id">
                             <template x-for="module in value.modules">
-                                <li x-text="module.title" :data-id="module.id"></li>
+                                <li class="p-1 bg-white rounded border-2 border-inherit font-medium" x-text="module.title" :data-id="module.id"></li>
                             </template>
                         </ul>
                     </li>
@@ -26,6 +26,8 @@
 
         <script>
             document.addEventListener('alpine:init', () => {
+
+
                 Alpine.data('sort', () => ({
                     list: [{
                             id: 1,
@@ -50,24 +52,45 @@
                             title: "Curriculum 2",
                             description: "Curriculum 1 description",
                             modules: [{
-                                id: 1,
+                                id: 4,
                                 title: "Lesson 1",
                                 type: 'lesson'
                             }, {
-                                id: 2,
+                                id: 5,
                                 title: "Quiz 1",
                                 type: 'quiz'
                             }, {
-                                id: 2,
+                                id: 6,
                                 title: "Lesson 3",
                                 type: 'lesson'
                             }]
                         }
                     ],
                     init() {
-                        // log the list
-                        console.log(this.list[0])
+                        // get all elemts by class name curriculum_modules
+
+
+                        setTimeout(() => {
+                            const elements = this.$refs.items.querySelectorAll('.curriculum_modules');
+                            elements.forEach((ele, i) => {
+                                Sortable.create(ele, {
+                                    group: 'curriculum_modules',
+                                    onEnd: (event) => {
+                                        let list = Alpine.raw(this.list);
+                                        console.log(event.oldIndex);
+                                        console.log(event.newIndex);
+                                        console.log(event.from.id);
+                                        console.log(event.to.id);
+                                        console.log(event.item.dataset.id);
+                                        console.log(event);
+                                    },
+                                });
+                            });
+                        }, 500);
+
+
                         Sortable.create(this.$refs.items, {
+                            handle: '.handle',
                             onEnd: (event) => {
                                 let list = Alpine.raw(this.list);
                                 let moved_step = list.splice(event.oldIndex, 1)[0]
